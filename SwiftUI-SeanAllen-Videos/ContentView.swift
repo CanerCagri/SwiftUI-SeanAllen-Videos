5cf68f746f82120ca5f12e88bd9170ce1a09d919
CanerCagri <canerrcagrii@gmail.com>
2022-07-29 16:15:24 +0300

    Listing custom data with image, title, upload date

--- SwiftUI-SeanAllen-Videos/ContentView.swift
+++ SwiftUI-SeanAllen-Videos/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var videos: [Video] = []
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(videos) { video in
+                
+                HStack(spacing: 2) {
+                    Image(video.imageName)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 90.0)
+                        .cornerRadius(10)
+                    
+                    Spacer().frame(width: 16)
+                    
+                    VStack(alignment: .leading) {
+                        Text(video.title)
+                            .fontWeight(.semibold)
+                            .lineLimit(2)
+                            .minimumScaleFactor(0.75)
+                        Spacer().frame(height: 5)
+                        Text(video.uploadDate)
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .padding(.bottom)
+                
+            }.navigationTitle(Text("Sean Allen Youtube"))
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(videos: VideoList.topTwelve)
     }
 }
