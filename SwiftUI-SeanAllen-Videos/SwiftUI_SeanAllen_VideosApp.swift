33985dc44556b726e7e3c1a54fc0eafe17adf9dc
CanerCagri <canerrcagrii@gmail.com>
2022-07-29 15:07:55 +0300

    Initial Commit

---
+++ SwiftUI-SeanAllen-Videos/SwiftUI_SeanAllen_VideosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SeanAllen_VideosApp.swift
+//  SwiftUI-SeanAllen-Videos
+//
+//  Created by Caner Çağrı on 29.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SeanAllen_VideosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
